399a56f0bbbb62402a28dfef376b3ed930001333
DESKTOP-1GO38TS\nguye <nguyenviethuynh1996.vih@gmail.com>
2018-03-09 19:34:54 +0700

    add entity framwork code first for data

---
+++ HuynhShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuynhShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        HuynhShopDbContext dbContext;
+        public HuynhShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new HuynhShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
--- HuynhShop.Model/Models/Product.cs
+++ HuynhShop.Model/Models/Product.cs
@@ -1,5 +1,4 @@
-﻿using HuynhShop.Model.Abstract;
-using System;
+﻿
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
